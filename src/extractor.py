6e976f1b4a74e7d7d3279c777f54e3c27f3a5f6b
iSan <sp6670280@gmail.com>
2026-03-08 15:13:48 -0600

    feat: extractor con ThreadPoolExecutor y guardado en JSON

--- src/extractor.py
+++ src/extractor.py
@@ -12,35 +12,51 @@ end_point = os.getenv("HN_TOP_STORIES")
 item = os.getenv("HN_ITEM")
 max_post = os.getenv("MAX_POSTS")
 
-def obtener_ids(cant_items: int):
+def obtener_ids(cant_items: int) -> list:
+    """Recibe los posts mas virales y 
+    nos quedamos con n cantidad"""
+    
     response = requests.get(f"{api}{end_point}").json()
     return response[:cant_items]
 
-def obtener_item(id: int):
+def obtener_item(id: int) -> json:
+    """Recibe el id del post y retorna su infromación"""
+    
     response = requests.get(f"https://hacker-news.firebaseio.com/v0/item/{id}.json?print=pretty")
     return response.json()
     
 def extraer(list_ids: list, cantidad: int) -> list:
+    """Hace 10 gets por seg y de cada id recibe la
+    información del post"""
+    
     with ThreadPoolExecutor(max_workers=10) as executor:
         items = list(executor.map(obtener_item, list_ids[:cantidad]))
     return items
 
-def guardar(items: list):
+def leer_datos():
+    """Intenta leer el archivo local, si no existe
+    renorta una lista vacía"""
     try:
         with open("data/raw/items.json", "r", encoding="utf-8") as f:
-            data = json.load(f)
-            data.extend(items)
-            with open("data/raw/items.json", "w", encoding="utf-8") as f:
-                return f.write(json.dumps(data, indent=4))
-            
-    except (FileNotFoundError, json.JSONDecodeError):
-        with open("data/raw/items.json", "w", encoding="utf-8") as f:
-            return f.write(json.dumps(items, indent=4))
-        
+            return json.load(f)       
+    except FileNotFoundError:
+        return []
     
+def guardar(items: list):
+    datos = leer_datos()
+    datos.extend(items)
+    with open("data/raw/items.json", "w", encoding="utf-8") as f:
+        f.write(json.dumps(datos, indent=2))
+        print("Items guardados:", json.dumps(items, indent=2))
+        
 def main():
     ids = obtener_ids(10)
-    items = extraer(list_ids=ids, cantidad=10)
+    items = extraer(list_ids=ids, cantidad=1)
     guardar(items)
 
 main()
+
+# Lecciones:
+"""Se usan en diferentes ocasiones"""
+#json.loads(f)  # ❌ f es un archivo, no un string
+#json.load(f)   # ✅ f es un archivo
